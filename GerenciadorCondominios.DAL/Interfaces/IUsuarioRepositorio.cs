4c90b5bd75662b720501aae0fa646119a4bd3569
IgorPRibeiro <igorpinheiroribeiro14@gmail.com>
2020-07-03 19:49:33 -0300

    Criando métodos do Usuario controller

--- GerenciadorCondominios.DAL/Interfaces/IUsuarioRepositorio.cs
+++ GerenciadorCondominios.DAL/Interfaces/IUsuarioRepositorio.cs
@@ -1,11 +1,23 @@
 ﻿using GerenciadorCondominios.BLL.Models;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GerenciadorCondominios.DAL.Interfaces
 {
     public interface IUsuarioRepositorio : IRepositorioGenerico<Usuario>
     {
+        int VerificarSeExisteRegistro();
+
+        //Task = assincrono
+        Task LogarUsuario(Usuario usuario, bool lembrar);
+        //Esse metodo retorna o IdentityResult
+        //IdentityResult = sera o status da operacao
+        //se der certo ira retorna um sucess e se der um erro retornara um fail,e assim, vai poder percorrer os erros que foram retornados
+        Task<IdentityResult> CriarUsuario(Usuario usuario, string senha);
+
+        Task IncluirUsuarioEmFuncao(Usuario usuario, string funcao);
     }
 }
--- GerenciadorCondominios.DAL/Repositorio/UsuarioRepositorio.cs
+++ GerenciadorCondominios.DAL/Repositorio/UsuarioRepositorio.cs
@@ -1,17 +1,82 @@
 ﻿using GerenciadorCondominios.BLL.Models;
 using GerenciadorCondominios.DAL.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GerenciadorCondominios.DAL.Repositorio
 {
     //RepositorioGenerico<Usuario>, IUsuarioRepositorio => usamos "," para mostrar qual interface usar
     public class UsuarioRepositorio : RepositorioGenerico<Usuario>, IUsuarioRepositorio
     {
+        private readonly Contexto _contexto;
+        //atributo para gerenciar as operacoes dos usuarios
+        private readonly UserManager<Usuario> _gerenciadorUsuario;
+        //atributo para gerenciar o Login
+        private readonly SignInManager<Usuario> _gerenciadorLogin;
+
+
         //Toda vez que a classe base gera um contexto precisamos gerar um contrutor que faca a mesma coisa
-        public UsuarioRepositorio(Contexto contexto) : base(contexto)
+        public UsuarioRepositorio(Contexto contexto, UserManager<Usuario> gerencadorUsuario, SignInManager<Usuario> gerenciarLogin) : base(contexto)
         {
+            _contexto = contexto;
+            _gerenciadorUsuario = gerencadorUsuario;
+            _gerenciadorLogin = gerenciarLogin;
+        }
+
+        public async Task<IdentityResult> CriarUsuario(Usuario usuario, string senha)
+        {
+            try
+            {
+                return await _gerenciadorUsuario.CreateAsync(usuario, senha);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task IncluirUsuarioEmFuncao(Usuario usuario, string funcao)
+        {
+            try
+            {
+                await _gerenciadorUsuario.AddToRoleAsync(usuario, funcao);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task LogarUsuario(Usuario usuario, bool lembrar)
+        {
+            try
+            {
+                await _gerenciadorLogin.SignInAsync(usuario, lembrar);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public int VerificarSeExisteRegistro()
+        {
+            try
+            {
+                return _contexto.Usuarios.Count();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
